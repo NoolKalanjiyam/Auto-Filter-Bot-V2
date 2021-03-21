602886d3867374e5778c52c6d797345594f561ce
NoolKalanjiyam <80931660+NoolKalanjiyam@users.noreply.github.com>
2021-03-21 20:55:47 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ class script(object):
     START_MSG = """<b>Hi {}</b>"""
 
 
-    GROUP_MSG = """<b></b>"""
+    GROUP_MSG = """url="https://t.me/pdfworld"""
 
-CHANNEL_MSG = """⭕️<b></b>⭕️"""
+CHANNEL_MSG = """⭕️url="https://t.me/tamilbooksworld⭕️"""

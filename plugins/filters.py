29edd6d10ea0741ca2a6b9c0927be85fb8515aee
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-28 08:12:19 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -179,23 +179,6 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             )
 
 
-   
-
-        elif query.data == "about_data":
-            await query.answer()
-            keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton("BACK", callback_data="help_data"),
-                    InlineKeyboardButton("START", callback_data="start_data")],
-                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")]
-            ])
-
-            await query.message.edit_text(
-                script.ABOUT_MSG,
-                reply_markup=keyboard,
-                disable_web_page_preview=True
-            )
-
-
         elif query.data == "delallconfirm":
             await query.message.delete()
             await deleteallfilters(client, query.message)
